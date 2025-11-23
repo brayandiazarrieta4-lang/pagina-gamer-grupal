5470dc89c45462661e9fb5f17cc3f3a75106fa20
Yeiner Stainer Moreno Escobar <ymescobar08@gmail.com>
2025-11-23 18:55:07 -0500

    feat: agregar gama de colores neon al proyecto

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import React from 'react'
 
 export default function Footer() {
     const current_year = new Date().getFullYear();
@@ -9,11 +8,11 @@ export default function Footer() {
             transition-all duration-500 hover:scale-105
             backdrop-blur-xl border border-white/10 '>
         <div className='text-center py-4'>
-            <Link href='https://gamingstoreym-55uyorh6b-yeiner-morenos-projects.vercel.app/privacy' target='_blank' className='text-purple-800 btn btn-link'>Privacy</Link>
+            <Link href='https://gamingstoreym-55uyorh6b-yeiner-morenos-projects.vercel.app/privacy' target='_blank' className='text-neon-purple text-glow-purple btn btn-link'>Privacy</Link>
             <span> | </span>
-            <Link href='https://gamingstoreym-55uyorh6b-yeiner-morenos-projects.vercel.app/terms' target='_blank' className='text-purple-800 btn btn-link'>Terms</Link>
+            <Link href='https://gamingstoreym-55uyorh6b-yeiner-morenos-projects.vercel.app/terms' target='_blank' className='text-neon-purple text-glow-purple btn btn-link'>Terms</Link>
             <span> | </span>
-            <Link href='mailto:Roads<yostingabriel036@gmail.com>' className='text-purple-800 btn btn-link'>Contact</Link>
+            <Link href='mailto:Roads<yostingabriel036@gmail.com>' className='text-neon-purple text-glow-purple btn btn-link'>Contact</Link>
             <p className='text-blue-900 text-ms'>Copyright &copy; {current_year}, GamingStore LLC, All Rights Reserved</p>
 
 
